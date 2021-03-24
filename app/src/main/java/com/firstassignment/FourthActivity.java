8b8176c37015bda5e8964d35bf8e773076925de3
BathiNtshinga <60979642+BathiNtshinga@users.noreply.github.com>
2021-03-24 11:26:07 +0200

    Initial Commit

--- app/src/main/java/com/firstassignment/FourthActivity.java
+++ app/src/main/java/com/firstassignment/FourthActivity.java
@@ -3,12 +3,32 @@ package com.firstassignment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class FourthActivity extends AppCompatActivity {
+
+    String description = "Passing Messages from Third Activity ";
+    EditText editText;
+    Button btn = (Button) findViewById(R.id.button);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fourth);
+
+        editText= (EditText) findViewById(R.id.editText);
+        btn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                String namevalue = editText.getText().toString();
+                Intent intent = new Intent(FourthActivity.this, FifthActivity.class);
+                intent.putExtra("Name",namevalue);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/firstassignment/SecondActivity.java
+++ app/src/main/java/com/firstassignment/SecondActivity.java
@@ -3,8 +3,14 @@ package com.firstassignment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SecondActivity extends AppCompatActivity {
+
+    String description = "Passing Messages from  ";
+    EditText editText;
+    Button btn = (Button) findViewById(R.id.button);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

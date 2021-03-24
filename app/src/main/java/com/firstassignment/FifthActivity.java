1014f1692e180ddc2b46a9ac90efb4a0438e39de
BathiNtshinga <60979642+BathiNtshinga@users.noreply.github.com>
2021-03-24 07:34:28 +0200

    Initial commit

---
+++ app/src/main/java/com/firstassignment/FifthActivity.java
@@ -0,0 +1,20 @@
+package com.firstassignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class FifthActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fifth);
+    }
+}

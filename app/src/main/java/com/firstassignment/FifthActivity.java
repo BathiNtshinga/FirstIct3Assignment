e1f3b12025bec7feab1755f547b8c581164f20ce
BathiNtshinga <60979642+BathiNtshinga@users.noreply.github.com>
2021-03-24 10:43:20 +0200

    Initial Commit

--- app/src/main/java/com/firstassignment/FifthActivity.java
+++ app/src/main/java/com/firstassignment/FifthActivity.java
@@ -10,11 +10,23 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class FifthActivity extends AppCompatActivity {
-
+    String description = "Passing Messages to Activities";
+    EditText editText;
+    Button btn = (Button) findViewById(R.id.button);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fifth);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String namevalue = editText.getText().toString();
+                Intent intent = new Intent(FifthActivity.this,MainActivity.class);
+                intent.putExtra("Name",namevalue);
+                startActivity(intent);
+            }
+        });
     }
 }
